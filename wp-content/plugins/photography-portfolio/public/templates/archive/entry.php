1e2cae8e9e6d1fa6bfcfaa2a78879c099e6a03f8
Andrea Venturelli <andrea.venturelli@mikamai.com>
2018-02-23 18:06:06 +0100

    change cat

--- wp-content/plugins/photography-portfolio/public/templates/archive/entry.php
+++ wp-content/plugins/photography-portfolio/public/templates/archive/entry.php
@@ -33,7 +33,7 @@
 	<a class="PP_Entry__more" href="<?php the_permalink(); ?>">
 	<span class="PP_Entry__inner">
 		<span class="PP_Entry__view">
-			<?php esc_html_e( 'View Gallery', 'photography-portfolio' ) ?>
+			<?php esc_html_e( 'Read more', 'photography-portfolio' ) ?>
 		</span>
 	</span>
 	</a> <!-- .PP_Entry__more -->
